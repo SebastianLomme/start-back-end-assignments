76ca09c0942e7739f96e47262514671040289af9
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-10-25 19:26:11 +0200

    Assignment Arguments added

---
+++ arguments/main.py
@@ -0,0 +1,33 @@
+# Do not modify these lines
+__winc_id__ = "7b9401ad7f544be2a23321292dd61cb6"
+__human_name__ = "arguments"
+
+# Add your code after this line
+
+
+def greet(name, template="Hello, <name>!"):
+    return template.replace("<name>", name)
+
+
+def force(mass, body="earth"):
+    gravity = {
+        "sun": 274,
+        "jupiter": 24.92,
+        "neptune": 11.15,
+        "saturn": 10.44,
+        "earth": 9.798,
+        "uranus": 8.87,
+        "venus": 8.87,
+        "mars": 3.71,
+        "mercury": 3.7,
+        "moon": 1.62,
+        "pluto": 0.58,
+    }
+    force = mass * round(gravity[body], 1)
+    return round(force, 2)
+
+def pull(m1, m2, d):
+    G = 6.674 * 10**-11
+    return G * ((m1 * m2) /d**2)
+
+print(pull(800, 1500, 3))
